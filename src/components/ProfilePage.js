406138c277f489f8e8ba2f6a2fd346897332663e
Tigran <tigrancho2000@gmail.com>
2021-05-25 19:16:43 +0400

    laptop to pc

--- src/components/ProfilePage.js
+++ src/components/ProfilePage.js
@@ -1,35 +1,59 @@
+import { makeStyles } from "@material-ui/core";
 import React, { useEffect } from "react";
 import { Link, useHistory } from "react-router-dom";
 import doctorAPI from "../api/doctor.api.js";
 
-export default function ProfilePage() {
-	const [name, setName] = React.useState("");
-	const [surname, setSurname] = React.useState("");
+const useStyles = makeStyles({
+	doctorContainer: {
+		display: "flex",
+	},
+	image: {
+		width: "240px",
+		height: "300px",
+	},
+});
 
+export default function ProfilePage() {
+	const [doctor, setDoctor] = React.useState({});
+	const [firstLogin, setFirstLogin] = React.useState(false);
+
+	const classes = useStyles();
 	const history = useHistory();
 
 	useEffect(() => {
 		doctorAPI.getProfile().then((res) => {
-			console.log(res);
 			if (res === 403) return history.push("/");
 			if (res === 404) return history.push("/");
 			if (res === 409) return history.push("/admin/profile");
 
-			setName(res.data.name);
-			setSurname(res.data.surname);
+			setDoctor(res.data);
+
+			if (res.data.firstLogin) setFirstLogin(true);
 		});
 	}, [history]);
 
-	return (
+	return doctor.name ? (
 		<>
 			<div>
-				<h1>
-					Welcome back, {name} {surname}!
-				</h1>
-				<Link to="/patients">Patients Page</Link>
-				<br />
-				<Link to="/edit">Edit account</Link>
+				{firstLogin && (
+					<p>
+						Welcome to medical center! Please update your password for security
+						reasons. <Link to="edit/password">Edit password</Link>
+					</p>
+				)}
+				<div className={classes.doctorContainer}>
+					<img
+						src={`http://localhost:8000/images/${doctor.photo}`}
+						alt="Doctor"
+						className={classes.image}
+					/>
+					<h1>
+						Welcome, {doctor.name} {doctor.surname}!
+					</h1>
+				</div>
 			</div>
 		</>
+	) : (
+		<></>
 	);
 }
